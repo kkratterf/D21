247676e304d5a3859ca237a65a550feefb6e1c07
Federico Kratter Thaler <kkratterf@gmail.com>
2025-07-03 08:23:10 +0200

    fix: badge

--- apps/web/app/(external)/s/layout.tsx
+++ apps/web/app/(external)/s/layout.tsx
@@ -16,7 +16,7 @@ export default async function MainLayout({
             <main className='!ml-1.5 mt-2 h-[calc(100dvh-8px)] w-full overflow-auto rounded-tl-3xl border border-t border-l bg-background shadow-sm md:ml-0'>
                 {children}
             </main>
-            <div className='fixed bottom-6 left-7 z-50 w-fit rounded-[10px] border border-default bg-elevated p-px shadow-lg'>
+            <div className='fixed bottom-[72px] left-7 z-50 w-fit rounded-[10px] border border-default bg-elevated p-px shadow-lg'>
                 <div className='flex h-6 items-center rounded-lg border border-default bg-background px-1.5 py-1'>
                     <span className='text-description text-xs'>Designed by <Link className='text h-4 hover:text-brand' href='https://www.linkedin.com/in/kkratter/' target='_blank'>Federico</Link></span>
                 </div>
